0ae64c34d4f43078b85bf097032054c98995ec05
Murillo Conti <murilloconti@hotmail.com>
2019-05-17 14:43:23 -0300

    Setting up apollo

---
+++ thestartupfestvue/src/components/Shared/Card.vue
@@ -0,0 +1,40 @@
+<template>
+  <a v-on:click="changeToVote">
+    <div class="card shadow rounded text-center mt-4">
+      <div class="card-header p-0">
+        <img :src="ImgUrl" alt class="img-fluid p-1">
+      </div>
+      <div class="card-body">
+        <p class="card-title fest-title">{{CardTitle}}</p>
+        <p class="card-text fest-text">{{CardText}}</p>
+      </div>
+    </div>
+  </a>
+</template>
+<script>
+export default {
+  name: "Card",
+  props: {
+    CardTitle: String,
+    CardText: String,
+    ImgUrl: String,
+    SegmentId: String
+  },
+  methods: {
+    changeToVote: function() {        
+      this.$router.push({ name: 'voting', params: { Id: this.SegmentId } })
+    }
+  }
+};
+</script>
+<style>
+.fest-title {
+  font-weight: bold;
+  font-size: 1.5em;
+  color: #424242;
+}
+.fest-text {
+  font-weight: bold;
+  color: #757575;
+}
+</style>
--- thestartupfestvue/src/main.js
+++ thestartupfestvue/src/main.js
@@ -1,9 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import apollo from './apolloClient' 
 
 Vue.config.productionTip = false;
-
+Vue.prototype.$apollo = apollo 
 new Vue({
   router,
   render: h => h(App)
