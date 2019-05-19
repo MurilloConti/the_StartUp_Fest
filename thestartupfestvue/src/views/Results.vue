4c4cafaf67c3ba09c0a6e17e151ee81a13a139d9
Murillo Conti <murilloconti@hotmail.com>
2019-05-19 14:39:23 -0300

    Added home button  on result View

--- thestartupfestvue/src/views/Results.vue
+++ thestartupfestvue/src/views/Results.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col text-center m-3 sticky-top">
-        <h2>Resultados</h2>
-      </div>
+      <nav class="navbar fixed-top navbar-light bg-light justify-content-center align-middle p-3 shadow-sm">
+        <div class="col-1">
+           <router-link to="/home" style="color:#424242"><i class="fas fa-home"></i></router-link>
+         </div>        
+        <div class="col pt-2 d-flex align-items-center justify-content-center"><h4 style="color:#424242;font-weight:bold">Resultados</h4></div>
+      </nav>
     </div>
     <ResultTable TableTitle="Proposta" :rows="this.$store.state.propostas"></ResultTable>
     <ResultTable TableTitle="Apresentação/pitch" :rows="this.$store.state.apresentacoes"></ResultTable>
